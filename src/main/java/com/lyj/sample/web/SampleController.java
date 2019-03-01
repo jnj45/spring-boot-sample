0c8b1c398cae85d5d24663fdb920792b398aec8e
LYJ <LYJ@DESKTOP-126K8GO>
2019-03-01 10:49:46 +0900

    .

---
+++ src/main/java/com/lyj/sample/web/SampleController.java
@@ -0,0 +1,13 @@
+package com.lyj.sample.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("/hello.do")
+	public String sayHello() {
+		return "Hello!";
+	}
+}
